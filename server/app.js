bb3425a979081a516f85da4a06013047de33a34b
smoha020 <smoha020@ucr.edu>
2021-12-05 12:04:20 -0800

    minor fixes

--- server/app.js
+++ server/app.js
@@ -3,18 +3,15 @@ const app = express();
 const peopleController = require('./controllers/people.js');
 const mongoose = require('mongoose');
 
+/*Because client and backend are communicating 
+from different ports*/
 const cors = require('cors');
 
-//Because of different ports
 app.use(cors());
 
-//const db = require('./index.js')
-//parser
+//parses (formats) the data so we can use it
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
- 
-//connect to db 
-//db.connect();
 
 
 app.get('/api1/getAll', peopleController.getPeople)
@@ -25,10 +22,10 @@ let uri = '';
 const PORT = process.env.PORT || 5000;
 
 if (process.env.MONGO_DB_APP) {
-    //
+    //when running Docker
     uri = process.env.MONGO_DB_APP
 } else {
-    //
+    //no Docker
     uri = 'mongodb://localhost:27017/monoAppDB' 
 }
 
